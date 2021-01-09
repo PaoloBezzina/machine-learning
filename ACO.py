451e4c1f311ede00a45e9a14c86b06d7b9c66050
Paolo Bezzina <paolo.bezzina.19@um.edu.mt>
2021-01-09 18:51:43 +0100

    final commit

--- ACO.py
+++ ACO.py
@@ -1,26 +1,30 @@
+#importing needed libraries
 import tsplib95
 import acopy
 import networkx as nx
 import matplotlib.pyplot as plt
 import time
 
+#set timer start time
 start_time = time.time()
 
+#setting setup variables
 solver = acopy.Solver(rho=.03, q=1)
 colony = acopy.Colony(alpha=1, beta=3)
 
+#load problem from file
 problem = tsplib95.load('machine-learning/Instances/ch150.tsp')
 G = problem.get_graph()
 
+#calling function from library to solve TSP
 tour = solver.solve(G, colony, limit=100)
 
+#Printing result information
+print("Best TSP tour length: %d" %tour.cost)
+print("Best TSP tour: %s" %tour.path)
 print("Time taken: %s seconds" %(time.time() - start_time))
-print("Tour Distance: %d" %tour.cost)
-print("Tour Path Taken: %s" %tour.path)
 
-#plt.plot(tour.path)
-
-#draw graph
+#sketching the graph for the resulting TSP
 _, ax = plt.subplots()
 pos = problem.display_data or problem.node_coords
 nx.draw_networkx_nodes(G, pos=pos, ax=ax, node_color="brown")
@@ -33,6 +37,3 @@ nx.draw_networkx_edges(G, pos=pos, edgelist=path, edge_color="black")
 # If this doesn't exsit, x_axis and y_axis's numbers are not there.
 ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
 plt.show()
-
-
-
--- SA.py
+++ SA.py
@@ -1,36 +1,28 @@
-#from _future_ import division
+#importing necessary libraries
 import tsplib95
 import time
 from satsp import solver
 
-import networkx as nx
-import matplotlib.pyplot as plt
-import numpy as np
+#load problem from file
+problem = tsplib95.load('machine-learning/Instances/burma14.tsp')
 
-problem = tsplib95.load('machine-learning/Instances/ch150.tsp')
-
+#set timer start time
 start_time = time.time()
 
-print(problem.is_depictable())
-print(problem.display_data_type)
-
+#reading nodes/coords accordingly depending on the type defined in the file
 cities = []
 if(problem.display_data_type == "TWOD_DISPLAY"):
     data = problem.display_data
 else:
     data = problem.node_coords
 
-print(len(data))
+#adding all nodes to a list of cities to be travalled to
 for i in range(0, len(data)):
     print(i+1, data[i+1][0], data[i+1][1])
     other_list = [i+1, data[i+1][0], data[i+1][1]]
     cities.append(other_list)
 
-
-#solver.Solve(cities,epochs=10000)
-solver.Solve(cities, stopping_count=300)
+#calling the function to solve the tsp from the library SAtsp
+solver.Solve(cities, stopping_count=300)    #solver.Solve(cities,epochs=10000)
 solver.PrintSolution()
-
 print("Time taken: %s seconds" %(time.time() - start_time))
-print("Best Tour Distance: %.6f" %solver.GetBestDist())
-print("Tour Path Taken: %s" %solver.GetBestTour())
